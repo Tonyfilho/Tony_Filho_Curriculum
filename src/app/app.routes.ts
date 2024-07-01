9c038f6e7780e9929baaee1a98f95d312bf2fb5a
tonyfilho <oficinadosdrones@gmail.com>
2024-07-01 17:09:23 +0100

    signin.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,10 @@ import { BodyComponent } from './body/body.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'body', pathMatch: 'full' },
-  {path:"about", loadComponent: ()  => import('./pages/about/about.component').then(m => m.AboutComponent)},
-  {path:"not-found", component: NotFoundComponent},
-  {path:"body", component: BodyComponent},
-  { path: '**', redirectTo: () => { return '/not-found'} },
+  { path: "about", loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
+  { path: "autentication", loadComponent: () => import('./pages/signin/signin.component').then(m => m.SigninComponent)},
+  { path: "not-found", component: NotFoundComponent },
+  { path: "body", component: BodyComponent },
+  { path: '**', redirectTo: () => { return '/not-found' } },
 
 ];
--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
