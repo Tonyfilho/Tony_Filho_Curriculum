2a45c178a1b1cd9209ee9b6f2bbda091d6c19750
tonyfilho <oficinadosdrones@gmail.com>
2024-07-03 16:36:11 +0100

    formcontrols

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,23 +1,38 @@
-import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
-export class SigninComponent {
-
-
+export class SigninComponent  implements OnInit{
+  private emailRegex:RegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  private fb = inject(UntypedFormBuilder);
   route = inject(Router);
+  autenticationForm!: UntypedFormGroup ;
 
+
+  ngOnInit(): void {
+    this.autenticationForm = this.fb.group({
+         email: ['',  {validators: [Validators.required, Validators.pattern(this.emailRegex) ], updateOn: 'blur'}],
+         password: ['', {validators: [Validators.required, Validators.minLength(8), Validators.maxLength(16)], updateOn: 'blur'}]
+
+    });
+
+  }
 
 
   goBack() {
     this.route.navigateByUrl("/")
+
+  }
+
+  submitForms() {
+    
 
   }
 
