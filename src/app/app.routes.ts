a6c6e77f95111b4582419dd59f19ed75497c97be
tonyfilho <oficinadosdrones@gmail.com>
2024-07-26 15:30:21 +0100

    home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,11 @@ import { BodyComponent } from './body/body.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'body', pathMatch: 'full' },
-  { path: "about", loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
-  { path: "autentication", loadComponent: () => import('./pages/signin/signin.component').then(m => m.SigninComponent)},
-  { path: "not-found", component: NotFoundComponent },
+  { path: "about", loadComponent: () => import('./pages/about/about.component').then(c => c.AboutComponent) },
+  { path: "autentication", loadComponent: () => import('./pages/signin/signin.component').then(c => c.SigninComponent)},
+  { path:"home", loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)},
   { path: "body", component: BodyComponent },
+  { path: "not-found", component: NotFoundComponent },
   { path: '**', redirectTo: () => { return '/not-found' } },
 
 ];
