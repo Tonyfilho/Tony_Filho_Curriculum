12bf0c6a95bbc88817f248eb8ea08353e33ebc68
tonyfilho <oficinadosdrones@gmail.com>
2024-08-06 16:20:25 +0100

    http-interceptor not woking

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,18 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { environment } from '../environments/environment.prod';
 
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { HttpAuthenticationInterceptorService } from './pages/signin/services/http-authentication-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    {  provide:  HTTP_INTERCEPTORS, useClass: HttpAuthenticationInterceptorService, multi: true  },
     provideRouter(routes),
     provideAnimationsAsync(),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
---
+++ src/app/pages/signin/services/http-authentication-interceptor.service.ts
@@ -0,0 +1,46 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpParams, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { exhaustMap, Observable, take } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+/**
+ * 5º Reportar no Providers[...], nosso Interceptor, {
+   provide: HTTP_INTERCEPTORS, useClass: HttpAuthenticationInterceptorService, multi: true
+  }
+ */
+export class HttpAuthenticationInterceptorService implements HttpInterceptor {
+
+
+  constructor(private auth: AuthenticationService) {
+
+
+  }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return this.auth.tokenResponse$.pipe(take(1), exhaustMap(response => {
+      console.log("tokenResponse$; ", response);
+
+      if (!response) {
+        console.log("No Response; ", response);
+        return next.handle(req);
+
+      }
+      /**Clonando o request , pois original não pode ser SETADO, é IMUTÁVEL */
+      const modifiedHeader = req.clone({
+        // setHeaders: {
+        //   Authorization: response.idToken
+        // }
+        /**Ou */
+
+        params: new HttpParams().set('auth', response.idToken)
+      });
+      console.log("modifiedHeader; ", modifiedHeader);
+      return next.handle(modifiedHeader);
+
+    }));
+
+
+  }
+}
