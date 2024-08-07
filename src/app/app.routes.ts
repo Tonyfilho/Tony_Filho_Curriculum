15f19e5fb97b8e7fecaa40a087eae3961ef631d9
tonyfilho <oficinadosdrones@gmail.com>
2024-08-07 09:17:42 +0100

    teste in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { BodyComponent } from './body/body.component';
 import { SigninComponent } from './pages/signin/signin.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 
 export const routes: Routes = [
@@ -11,9 +13,11 @@ export const routes: Routes = [
   { path: "body", component: BodyComponent },
   { path: "autentication", component: SigninComponent },
   { path: "not-found", component: NotFoundComponent },
-  { path: "about", loadComponent: () => import('./pages/about/about.component').then(c => c.AboutComponent) },
+  { path:"home", component: HomeComponent},
+  { path: "about", component: AboutComponent },
+  // { path: "about", loadComponent: () => import('./pages/about/about.component').then(c => c.AboutComponent) },
   // { path: "autentication", loadComponent: () => import('./pages/signin/signin.component').then(c => c.SigninComponent)},
-  { path:"home", loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)},
+  // { path:"home", loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)},
   { path: '**', redirectTo: () => { return '/not-found' } },
 
 ];
