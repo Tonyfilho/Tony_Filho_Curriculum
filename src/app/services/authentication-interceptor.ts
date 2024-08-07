dbddb29e602896f9d2173cc9da77442d736b414c
tonyfilho <oficinadosdrones@gmail.com>
2024-08-07 16:43:55 +0100

    httpinterceptor testing

--- src/app/services/authentication-interceptor.ts
+++ src/app/services/authentication-interceptor.ts
@@ -5,23 +5,42 @@ import { AuthenticationService } from "./authentication.service";
 
 export const interceptorFN: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthenticationService);
-  console.log("dentro do interceptor");
+ 
 
   //return authService.tokenResponse$.pipe(take(1), exhaustMap(token => {
-  return authService.tokenResponse$.pipe(take(2),switchMap(token => {
-    console.log("dentro do interceptor", token?.localId);
-    if (!token) {
-      return next(req);
-     }
-     const modifiedHeader = req.clone({
-     // params: new HttpParams().set('auth', token.idToken)
-     setHeaders: {
-      Authorization: token.idToken
-    }
-     })
-     return next(modifiedHeader);
-  }));
+ // return authService.tokenResponse$.pipe(take(1),switchMap(token => {
+  //   console.log("dentro do interceptor", token?.localId);
+  //   if (!token) {
+  //     return next(req);
+  //    }
+  //    const modifiedHeader = req.clone({
+  //    // params: new HttpParams().set('auth', token.idToken)
+  //    setHeaders: {
+  //     Authorization:`Bearer ${token.idToken}`
+  //   }
+  //    })
+  //    return next(modifiedHeader);
+  // }));
 
+
+  console.log('Interceptor chamado');
+
+  return authService.tokenResponse$.pipe(
+    take(1),
+    switchMap(token => {
+      console.log('Token recebido:', token);
+      if (!token) {
+        return next(req);
+      }
+      const modifiedHeader = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token.idToken}`
+        }
+      });
+      console.log('Requisição modificada:', modifiedHeader);
+      return next(modifiedHeader);
+    })
+  );
 
 
 }
