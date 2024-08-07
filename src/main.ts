f5dc67934ac6e91e8a8dcc1e10eb7d0557676af1
tonyfilho <oficinadosdrones@gmail.com>
2024-08-07 08:47:28 +0100

    create a FN class and MTD class

--- src/main.ts
+++ src/main.ts
@@ -11,20 +11,22 @@ import { environment } from './environments/environment';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
-bootstrapApplication(AppComponent, {
-  providers: [
-    // provideHttpClient(withInterceptorsFromDi()),
-    // {  provide:  HTTP_INTERCEPTORS, useClass: HttpAuthenticationInterceptorService, multi: true  },
-    provideHttpClient(withInterceptors([interceptorFN])),
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
-
-
-  ]
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+// bootstrapApplication(AppComponent, {
+//   providers: [
+// provideHttpClient(withInterceptorsFromDi()),
+// {  provide:  HTTP_INTERCEPTORS, useClass: HttpAuthenticationInterceptorService, multi: true  },
+//     provideHttpClient(withInterceptors([interceptorFN])),
+//     provideRouter(routes),
+//     provideAnimationsAsync(),
+//     provideFirebaseApp(() => initializeApp(environment.firebase)),
+//     provideAuth(() => getAuth()),
+//     provideFirestore(() => getFirestore())
+
+
+//   ]
+// }).catch((err) => console.error(err));
+
