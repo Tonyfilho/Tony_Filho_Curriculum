77e3036118977c1622bb57d3d5910916ecd72d50
tonyfilho <oficinadosdrones@gmail.com>
2024-08-13 11:23:35 +0100

    update header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,22 +2,18 @@
 
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { BodyComponent } from './body/body.component';
-import { SigninComponent } from './pages/signin/signin.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
+
+import { BodyComponent } from './components/body.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'body', pathMatch: 'full' },
   { path: "body", component: BodyComponent },
-  { path: "autentication", component: SigninComponent },
   { path: "not-found", component: NotFoundComponent },
-  { path:"home", component: HomeComponent},
-  { path: "about", component: AboutComponent },
-  // { path: "about", loadComponent: () => import('./pages/about/about.component').then(c => c.AboutComponent) },
-  // { path: "autentication", loadComponent: () => import('./pages/signin/signin.component').then(c => c.SigninComponent)},
-  // { path:"home", loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)},
+  { path: "about", loadComponent: () => import('./pages/about/about.component').then(c => c.AboutComponent) },
+  { path: "autentication", loadComponent: () => import('./pages/signin/signin.component').then(c => c.SigninComponent)},
+  { path: "register", loadComponent: () => import('./pages/register/register.component').then(c => c.RegisterComponent)},
+  { path:"home", loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)},
   { path: '**', redirectTo: () => { return '/not-found' } },
 
 ];
