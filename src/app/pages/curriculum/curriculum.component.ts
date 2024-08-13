105d1cfc07be89602f87a3f3bcbd76efa02ae0cb
tonyfilho <oficinadosdrones@gmail.com>
2024-08-13 11:32:35 +0100

    create firebaseAuth, curriculum component

---
+++ src/app/pages/curriculum/curriculum.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-curriculum',
+  standalone: true,
+  imports: [],
+  templateUrl: './curriculum.component.html',
+  styleUrl: './curriculum.component.scss'
+})
+export class CurriculumComponent {
+
+}
