4e7e2a060dbd7b720d90280b672dcf1d6d61d46f
tonyfilho <oficinadosdrones@gmail.com>
2024-08-30 18:52:10 +0100

    canActivate: [routesGuard]

---
+++ src/app/_services/routes-user.guard.ts
@@ -0,0 +1,18 @@
+import { AuthenticationService } from './authentication.service';
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+
+export const routesUserGuard: CanActivateFn = (route, state) => {
+const authenticationService = inject(AuthenticationService);
+let localLogin : boolean = false;
+
+authenticationService.user$.subscribe(hasUser => {
+   if (hasUser) {
+     return localLogin = true;
+   }
+    return localLogin;
+   }
+  );
+
+  return localLogin;
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 import { BodyComponent } from './components/body.component';
+import { routesUserGuard } from './_services/routes-user.guard';
 
 
 export const routes: Routes = [
@@ -13,7 +14,7 @@ export const routes: Routes = [
   { path: "about", loadComponent: () => import('./pages/about/about.component').then(c => c.AboutComponent) },
   { path: "autentication", loadComponent: () => import('./pages/signin/signin.component').then(c => c.SigninComponent)},
   { path: "register", loadComponent: () => import('./pages/register/register.component').then(c => c.RegisterComponent)},
-  { path:"home", loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)},
+  { path:"home", loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),  canActivate: [routesUserGuard],},
   { path: '**', redirectTo: () => { return '/not-found' } },
 
 ];
