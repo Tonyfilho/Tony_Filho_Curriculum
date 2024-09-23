b53efbc0372db518306d8d238125ed53c7364f2c
tonyfilho <oficinadosdrones@gmail.com>
2024-09-23 11:46:13 +0100

    fixing the body component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,12 +9,12 @@ import { routesGDPRGuard, routesUserGuard } from './_guards/routes-user.guard';
 
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: '', redirectTo: 'body', pathMatch: 'full' },
   { path: "not-found", component: NotFoundComponent },
   { path: "about", loadComponent: () => import('./pages/about/about.component').then(c => c.AboutComponent) },
   { path: "register", loadComponent: () => import('./pages/register/register.component').then(c => c.RegisterComponent)},
   { path: "recouver", loadComponent: () => import('./pages/recouver/recouver.component').then(c => c.RecouverComponent)},
-  { path: "body", loadComponent: () => import('./components/body.component').then(c => c.BodyComponent),  canActivate: [routesUserGuard] },
+  { path: "body", loadComponent: () => import('./components/body.component').then(c => c.BodyComponent) },
   { path: "autentication", loadComponent: () => import('./pages/signin/signin.component').then(c => c.SigninComponent), canActivate: [routesGDPRGuard]},
   { path:"home", loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),  canActivate: [routesUserGuard],},
   { path: '**', redirectTo: () => { return '/not-found' } },
